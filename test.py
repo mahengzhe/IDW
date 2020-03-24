e6392876faa72ccd8b467548f3d61b9bafbd2bdf
mahengzhe <51161713+mahengzhe@users.noreply.github.com>
2020-03-23 23:45:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import math
-
+import copy
 import matplotlib.pyplot as plt
 import numpy as np
-
+import time
 
 def interpolation(lon: np.ndarray, lat: np.ndarray, lst: list) -> np.ndarray :
     a = lon.size
@@ -13,142 +13,144 @@ def interpolation(lon: np.ndarray, lat: np.ndarray, lst: list) -> np.ndarray :
         for j in range(len(lat)) :
             c = lon[i]
             v = lat[j]
-            lst = selectzone(c, v, lst)  # select the significant zone to reduce computation
-            q = getIDW(lon[i], lat[j], lst)  # get IDW
-            z[i][j] = q  # z=s/t and load to the 2D array
 
+            lst1 = selectzone(c, v, lst)  # select the significant zone to reduce computation
+            s = 0
+            t = 0
+
+            if lst1 != None :
+                for k in range(len(lst1)) :
+
+                    d = distance(c, v, lst1[k])  # calculate the distance to all points
+                    # calculate as the z={∑[1/(d*d)]*z(i)}/∑[1/(d*d)]
+                    if d != 0 :
+                        s += (1 / float(d)) * lst1[k][2]  # s=z={∑[1/(d*d)]*z(i)}
+                        t += (1 / float(d))  # t=∑[1/(d*d)]
+                    else :
+                        s = lst1[k][2]
+                        t = 1
+
+                z[i][j] = s / t  # z=s/t and load to the 2D array
+            else:
+                z[i][j]=0
     return z  # get the Z array
 
 
-def selectzone(lon: int, lat: int, lst: list) -> list :  # determine the zone according to each point
+def selectzone(lon: float, lat: float, lst: list) -> list :  # determine the zone according to each point
+    lst0=copy.copy(lst)
+    if lst != None :
+        for k in range(len(lst0)) :
+            if lon == 0 and lat == 0 :  # at origin point
+                return lst0
+            elif lon == 0 and lat > 0 :  # at positive y-axis
 
-    if lon == 0 and lat == 0 :  # at origin point
-        return lst
-    elif lon == 0 and lat > 0 :  # at positive y-axis
-        for k in range(len(lst)) :
-            if lst[k][1] < 0 :
-                lst.remove(lst[k])
-            else :
-                if lst[k][0] != 0 :
-                    if lst[k][1] / lst[k][0] < 1 or lst[k][1] / lst[k][0] > -1 :
-                        lst.remove(lst[k])
+                if lst0[k][1] < 0 :
+                    lst.remove(lst0[k])
+                else :
+                    if lst0[k][0] != 0 :
+                        if lst0[k][1] / lst0[k][0] < 1 or lst0[k][1] / lst0[k][0] > -1 :
+                            lst0.remove(lst0[k])
 
-        return lst
-    elif lon == 0 and lat < 0 :  # at negative y-axis
-        for k in range(len(lst)) :
-            if lst[k][1] > 0 :
-                lst.remove(lst[k])
-            else :
-                if lst[k][0] != 0 :
-                    if lst[k][1] / lst[k][0] < 1 or lst[k][1] / lst[k][0] > -1 :
-                        lst.remove(lst[k])
+                return lst0
 
-        return lst
-    elif lon > 0 and lat == 0 :  # at positive x-axis
-        for k in range(len(lst)) :
-            if lst[k][0] < 0 :
-                lst.remove(lst[k])
-            else :
-                if lst[k][0] != 0 :
-                    if lst[k][1] / lst[k][0] > -1 or lst[k][1] / lst[k][0] < 1 :
-                        lst.remove(lst[k])
+            elif lon == 0 and lat < 0 :  # at negative y-axis
 
-        return lst
-    elif lon < 0 and lat == 0 :  # at negative x-axis
-        for k in range(len(lst)) :
-            if lst[k][0] > 0 :
-                lst.remove(lst[k])
-            else :
-                if lst[k][0] != 0 :
-                    if lst[k][1] / lst[k][0] > -1 or lst[k][1] / lst[k][0] < 1 :
-                        lst.remove(lst[k])
-        return lst
+                if lst0[k][1] > 0 :
+                    lst0.remove(lst0[k])
+                else :
+                    if lst0[k][0] != 0 :
+                        if lst0[k][1] / lst0[k][0] < 1 or lst0[k][1] / lst0[k][0] > -1 :
+                            lst0.remove(lst0[k])
 
-    else :  # not at any axis
-        if lon > 0 and lat > 0 :  # at the first quadrant
-            if lat / lon > 1 :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] < 0 or lst[k][0] < 0 or lst[k][1] < 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] < 1 :
-                            lst.remove(lst[k])
+                return lst0
+            elif lon > 0 and lat == 0 :  # at positive x-axis
 
-            else :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] < 0 or lst[k][0] or lst[k][1] < 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] > 1 :
-                            lst.remove(lst[k])
-            return lst
-        elif lon < 0 and lat > 0 :  # at the second quadrant
-            if lat / lon > -1 :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] > 0 or lst[k][1] < 0 or lst[k][0] > 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] < -1 :
-                            lst.remove(lst[k])
-            else :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] > 0 or lst[k][1] < 0 or lst[k][0] > 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] > -1 :
-                            lst.remove(lst[k])
-            return lst
-        elif lon < 0 and lat < 0 :  # at the third quadrant
-            if lat / lon > 1 :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] < 0 or lst[k][0] > 0 or lst[k][1] > 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] < 1 :
-                            lst.remove(lst[k])
-            else :
-                for k in range(len(lst)) :
-                    if lst[k][1] > 0 or lst[k][0] > 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] > 1 :
-                            lst.remove(lst[k])
-            return lst
-        elif lon > 0 and lat < 0 :  # at the fourth quadrant
-            if lat / lon > -1 :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] > 0 or lst[k][1] > 0 or lst[k][0] < 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] < -1 :
-                            lst.remove(lst[k])
-            else :
-                for k in range(len(lst)) :
-                    if lst[k][0] * lst[k][1] > 0 or lst[k][1] > 0 or lst[k][0] < 0 :
-                        lst.remove(lst[k])
-                    else :
-                        if lst[k][1] / lst[k][0] > -1 :
-                            lst.remove(lst[k])
-            return lst
+                if lst0[k][0] < 0 :
+                    lst0.remove(lst[k])
+                else :
+                    if lst0[k][0] != 0 :
+                        if lst0[k][1] / lst0[k][0] > -1 or lst0[k][1] / lst0[k][0] < 1 :
+                            lst0.remove(lst0[k])
 
+                return lst0
+            elif lon < 0 and lat == 0 :  # at negative x-axis
 
-def getIDW(x: float, y: float, lst: list) -> float :
-    s = 0
-    t = 0
-    if len(lst) == 0 :
-        return 0
+                if lst0[k][0] > 0 :
+                    lst0.remove(lst0[k])
+                else :
+                    if lst0[k][0] != 0 :
+                        if lst0[k][1] / lst0[k][0] > -1 or lst0[k][1] / lst0[k][0] < 1 :
+                            lst0.remove(lst0[k])
+                return lst
+
+            else :  # not at any axis
+                if lon > 0 and lat > 0 :  # at the first quadrant
+                    if lat / lon > 1 :
+
+                        if lst0[k][0] * lst0[k][1] < 0 or lst0[k][0] < 0 or lst0[k][1] < 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] < 1 :
+                                lst0.remove(lst0[k])
+
+                    else :
+
+                        if lst0[k][0] * lst0[k][1] < 0 or lst0[k][0] or lst0[k][1] < 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] > 1 :
+                                lst0.remove(lst0[k])
+                    return lst0
+                elif lon < 0 and lat > 0 :  # at the second quadrant
+                    if lat / lon > -1 :
+
+                        if lst0[k][0] * lst0[k][1] > 0 or lst0[k][1] < 0 or lst0[k][0] > 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] < -1 :
+                                lst0.remove(lst0[k])
+                    else :
+
+                        if lst0[k][0] * lst0[k][1] > 0 or lst0[k][1] < 0 or lst0[k][0] > 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] > -1 :
+                                lst0.remove(lst0[k])
+                    return lst0
+                elif lon < 0 and lat < 0 :  # at the third quadrant
+                    if lat / lon > 1 :
+
+                        if lst0[k][0] * lst0[k][1] < 0 or lst0[k][0] > 0 or lst0[k][1] > 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] < 1 :
+                                lst0.remove(lst0[k])
+                    else :
+
+                        if lst0[k][1] > 0 or lst0[k][0] > 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] > 1 :
+                                lst0.remove(lst0[k])
+                    return lst0
+                elif lon > 0 and lat < 0 :  # at the fourth quadrant
+                    if lat / lon > -1 :
+
+                        if lst0[k][0] * lst0[k][1] > 0 or lst0[k][1] > 0 or lst0[k][0] < 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] < -1 :
+                                lst0.remove(lst0[k])
+                    else :
+
+                        if lst0[k][0] * lst0[k][1] > 0 or lst0[k][1] > 0 or lst0[k][0] < 0 :
+                            lst0.remove(lst0[k])
+                        else :
+                            if lst0[k][1] / lst0[k][0] > -1 :
+                                lst0.remove(lst0[k])
+                    return lst0
     else :
-        for k in range(len(lst)) :
-            d = distance(x, y, lst[k])  # calculate the distance to all points
-            # calculate as the z={∑[1/(d*d)]*z(i)}/∑[1/(d*d)]
-            if d != 0 :
-                s += (1 / float(d)) * lst[k][2]  # s=z={∑[1/(d*d)]*z(i)}
-                t += (1 / float(d))  # t=∑[1/(d*d)]
-            else :
-                s = lst[k][2]
-                t = 1
-        return s / t
-
+        return lst
 
 # calculate the disrtance between 2 points
 def distance(p1: float, p2: float, pi: float) :
@@ -158,6 +160,7 @@ def distance(p1: float, p2: float, pi: float) :
 
 
 if __name__ == '__main__' :
+    start = time.clock()
     test = [[1, 1.252, 0.9234], [1.342, 1, 1.1543], [1, 1, 1.0136], [1, 0, 0.9126], [0, 1, 1.1034],
             [1.342, 0.23423, 0.9234], [0.2314, 0.324, 1.0445], [0.342, 0.234, 0.9422], [0.3452, 0.352, 0.956],
             [0.678, 0.735, 0.934],
@@ -171,4 +174,6 @@ if __name__ == '__main__' :
     cs = plt.contourf(x, y, Z)
     # plot the coutour
     plt.colorbar(cs)
+    end = time.clock()
+    print(end-start)
     plt.show()
