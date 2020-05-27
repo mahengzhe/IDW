5d02ac5516cbc47f26880a4223a17d3b8b3def57
mahengzhe <51161713+mahengzhe@users.noreply.github.com>
2020-05-27 21:59:45 +0800

    Update circle.py
    This program should be run as the Python 2.7 version.

--- circle.py
+++ circle.py
@@ -1,21 +1,24 @@
+#*- coding:UTF-8 -*-
 import math
 
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import pylab as p
+from matplotlib.mlab import griddata
 
-def distance(p1: float, p2: float, pi: float) :
+def distance(p1, p2, pi) :
     dis = (p1 - pi[0]) * (p1 - pi[0]) + (p2 - pi[1]) * (p2 - pi[1])
     m_result = math.sqrt(dis)
     return m_result
-def distance(p1: float, p2: float, pi: float) :
+def distance(p1, p2, pi) :
     dis = (p1 - pi[0]) ** 2 + (p2 - pi[1]) ** 2
     m_result = math.sqrt(dis)
     return m_result
 
 
-def out_in(lst: list, x: float, y: float, r=float) -> bool :
+def out_in(lst, x, y, r):
     for n in range(len(lst)) :
         d = distance(x, y, lst[n])
         if d > r :
@@ -24,7 +27,7 @@ def out_in(lst: list, x: float, y: float, r=float) -> bool :
             return True
 
 
-def circle(lst: list, r: float) :
+def circle(lst, r) :
     a = lst[0][0]
     b = lst[0][1]
 
@@ -52,24 +55,82 @@ def sort(a) :
     c = list(set(b))
     c.sort(key=b.index)
     return c
+#get the z for the points in the circle and expand from 1/8 circle to the whole circle
+def cal_z(c,l):
+    X=[]
+    Y=[]
+    Z=[]
+
+    for n in range(len(c)):
+      for m in range(len(l)):
+        #get the boundary for each y-axis
+        listx = []
+        a=c[n][0]
+        b=c[n][1]
+        x=l[m][0]
+        y=l[m][1]
+        #calculate the Z value the each point from the center the boundary
+        while x<=a:
+           s=0
+           t=0
+           z=0
+           x= round(x, 4)
+
+           b=round(b,2)
+           d=distance(x,y,c[n])
+           d=round(d,4)
+           if d != 0 :
+              s = s + (1 / (d * d)) * test[m][2]  # s=z={∑[1/(d*d)]*z(i)}
+              t = t + (1 / (d * d))  # t=∑[1/(d*d)]
+           else :
+              s = s + 0
+              t = s + 0
+
+           if t != 0 :
+              z = s / t  # z=s/t and load to the 2D array
+           else :
+              z = 0
+           z=round(z,4)
+           #add the the X,Y,Z as the list from first to fourth quadrant
+           listx.append((x, b, z))
+           listx.append((-x,b,z))
+           listx.append((-x, -b, z))
+           listx.append((x, -b, z))
+           listx.append((b, x, z))
+           listx.append((-b, x, z))
+           listx.append((-b, -x, z))
+           listx.append ((b, -x, z))
+           x=x+0.01
+        getX(listx,X)
+        getY(listx,Y)
+        getZ(listx,Z)
+
+    return X,Y,Z
+def getX(l,x):
+
+    for n in range(len(l)):
+        x.append(l[n][0])
+
+
+
+def getY(l, y)  :
+    for n in range(len(l)) :
+        y.append(l[n][1])
+
+
+
+def getZ(l, z)  :
+    for n in range(len(l)) :
+        z.append(l[n][2])
+
 
 
 if __name__ == '__main__' :
 
-    test = [[1, 1.252, 0.9234], [1.342, 1, 1.1543], [1, 1, 1.0136], [1, 0, 0.9126], [0, 1, 1.1034],
-            [0.678, 0.735, 0.934], [1.342, 0.23423, 0.9234], [0.2314, 0.324, 1.0445], [0.342, 0.234, 0.9422],
-            [0.3452, 0.352, 0.956],
+    test = [(1, 1.252, 0.9234), (1.342, 1, 1.1543), (1, 1, 1.0136), (1, 0, 0.9126), (0, 1, 1.1034),(0.678, 0.735, 0.934), (1.342, 0.23423, 0.9234), (0.2314, 0.324, 1.0445), (0.342, 0.234, 0.9422),(0.3452, 0.352, 0.956),(0.534, 0.9843, 1.048), (0.34, 0.98, 1.054)]
 
-            [0.534, 0.9843, 1.048], [0.34, 0.98, 1.054]]
 
-    # step = 0.01
-    # x = np.arange(-2, 2, step)
-    # y = np.arange(-2, 2, step)
-    # for i in range(len(x)):
-    #      for j in range(len(y)):
-
-    #        print(out_in(test,x[i],y[j],1.23))
-
+    #get the 1/8 circle and find the boundary 
     for n in range(len(test)) :
         s=0
         z=0
@@ -86,36 +147,21 @@ if __name__ == '__main__' :
         c=[(x[i],y[i]) for i in range(min(len(x),len(y)))]
         z=[]
         c=sort(c)
-        print(a,b,c)
-        listx=[]
-        for n in range(len(c)):
 
-            c1=c[n][0]
-            c2=c[n][1]
-            while a<=c[n][0]:
-                a = round(a, 2)
-                d=distance(a,c2,c[n])
-                d=round(d,4)
-                if d != 0 :
-                    s = s + (1 / (d * d)) * test[n][2]  # s=z={∑[1/(d*d)]*z(i)}
-                    t = t + (1 / (d * d))  # t=∑[1/(d*d)]
-                else :
-                    s = s + 0
-                    t = s + 0
 
-                if t != 0 :
-                   z = s / t  # z=s/t and load to the 2D array
-                else :
-                   z = 0
-                z=round(z,4)
-                listx.append((a,c2,z))
-                listx.append((-a,c2,z))
-                listx.append((-a, -c2, z))
-                listx.append((a, -c2, z))
-                listx.append((c2, a, z))
-                listx.append((-c2, a, z))
-                listx.append((-c2, -a, z))
-                listx.append((c2, -a, z))
-                a=a+0.01
 
-            print(listx)
+        X,Y,Z=cal_z(c,test)
+
+
+
+    p.scatter(X, Y, color='k')
+
+    # define grid.
+    xi = np.linspace(-2, 2, 1000)
+    yi = np.linspace(-2, 2, 1000)
+    zi = griddata(X, Y, Z, xi, yi,interp='linear')
+    cs = p.contour(xi, yi, zi, 10)
+    p.clabel(cs, inline=1, fontsize=12)
+    p.colorbar()
+
+    p.show()
