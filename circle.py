5904f21eb67d859ea4d766e5e3abc98de81af317
mahengzhe <51161713+mahengzhe@users.noreply.github.com>
2020-06-01 22:31:03 +0800

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,24 +1,22 @@
-#*- coding:UTF-8 -*-
 import math
 
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-import pylab as p
-from matplotlib.mlab import griddata
 
-def distance(p1, p2, pi) :
+
+def distance(p1 :float, p2: float, pi: float) :
     dis = (p1 - pi[0]) * (p1 - pi[0]) + (p2 - pi[1]) * (p2 - pi[1])
     m_result = math.sqrt(dis)
     return m_result
-def distance(p1, p2, pi) :
+def distance(p1: float, p2: float, pi: float) :
     dis = (p1 - pi[0]) ** 2 + (p2 - pi[1]) ** 2
     m_result = math.sqrt(dis)
     return m_result
 
 
-def out_in(lst, x, y, r):
+def out_in(lst: list, x: float, y: float, r,float) -> bool :
     for n in range(len(lst)) :
         d = distance(x, y, lst[n])
         if d > r :
@@ -27,7 +25,7 @@ def out_in(lst, x, y, r):
             return True
 
 
-def circle(lst, r) :
+def circle(lst: list, r: float) :
     a = lst[0][0]
     b = lst[0][1]
 
@@ -55,21 +53,20 @@ def sort(a) :
     c = list(set(b))
     c.sort(key=b.index)
     return c
-#get the z for the points in the circle and expand from 1/8 circle to the whole circle
-def cal_z(c,l):
+
+def cal_z(c:list,l:list)->list:
     X=[]
     Y=[]
     Z=[]
 
     for n in range(len(c)):
       for m in range(len(l)):
-        #get the boundary for each y-axis
         listx = []
         a=c[n][0]
         b=c[n][1]
         x=l[m][0]
         y=l[m][1]
-        #calculate the Z value the each point from the center the boundary
+
         while x<=a:
            s=0
            t=0
@@ -91,7 +88,6 @@ def cal_z(c,l):
            else :
               z = 0
            z=round(z,4)
-           #add the the X,Y,Z as the list from first to fourth quadrant
            listx.append((x, b, z))
            listx.append((-x,b,z))
            listx.append((-x, -b, z))
@@ -101,25 +97,26 @@ def cal_z(c,l):
            listx.append((-b, -x, z))
            listx.append ((b, -x, z))
            x=x+0.01
+        # put x,y,z in each list  
         getX(listx,X)
         getY(listx,Y)
         getZ(listx,Z)
 
     return X,Y,Z
-def getX(l,x):
+def getX(l:list,x:list)->list:
 
     for n in range(len(l)):
         x.append(l[n][0])
 
 
 
-def getY(l, y)  :
+def getY(l: list, y: list) -> list :
     for n in range(len(l)) :
         y.append(l[n][1])
 
 
 
-def getZ(l, z)  :
+def getZ(l: list, z: list) -> list :
     for n in range(len(l)) :
         z.append(l[n][2])
 
@@ -129,8 +126,14 @@ if __name__ == '__main__' :
 
     test = [(1, 1.252, 0.9234), (1.342, 1, 1.1543), (1, 1, 1.0136), (1, 0, 0.9126), (0, 1, 1.1034),(0.678, 0.735, 0.934), (1.342, 0.23423, 0.9234), (0.2314, 0.324, 1.0445), (0.342, 0.234, 0.9422),(0.3452, 0.352, 0.956),(0.534, 0.9843, 1.048), (0.34, 0.98, 1.054)]
 
+    # step = 0.01
+    # x = np.arange(-2, 2, step)
+    # y = np.arange(-2, 2, step)
+    # for i in range(len(x)):
+    #      for j in range(len(y)):
 
-    #get the 1/8 circle and find the boundary 
+    #        print(out_in(test,x[i],y[j],1.23))
+
     for n in range(len(test)) :
         s=0
         z=0
@@ -152,16 +155,24 @@ if __name__ == '__main__' :
 
         X,Y,Z=cal_z(c,test)
 
+    print(X)
+    print(Y)
+    print(Z)
+    # draw the scatter figure
+    fig = plt.figure()
+    ax1=plt.subplot(111,projection='3d')
+    
+    ax1.set_title('Scatter Plot')
+    
+    plt.xlabel('X')
+    
+    plt.ylabel('Y')
 
 
-    p.scatter(X, Y, color='k')
+    ax1.scatter(X[:40000], Y[:40000], Z[:40000], c='y')  # 绘制数据点
+    ax1.scatter(X[40000: 60000], Y[40000 :60000], Z[40000 :60000], c='r')
+    ax1.scatter(X[60000 :], Y[60000 :], Z[60000 :], c='g')
 
-    # define grid.
-    xi = np.linspace(-2, 2, 1000)
-    yi = np.linspace(-2, 2, 1000)
-    zi = griddata(X, Y, Z, xi, yi,interp='linear')
-    cs = p.contour(xi, yi, zi, 10)
-    p.clabel(cs, inline=1, fontsize=12)
-    p.colorbar()
+    #display
+    plt.show()
 
-    p.show()
